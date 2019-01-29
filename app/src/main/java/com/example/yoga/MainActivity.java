7062d946d1179bc9c60e347c88566ea53f664bcf
Mutharasan <muthu.ct.96@gmail.com>
2019-01-29 22:41:02 +0530

    Finished designing th fron page

--- app/src/main/java/com/example/yoga/MainActivity.java
+++ app/src/main/java/com/example/yoga/MainActivity.java
@@ -1,23 +1,22 @@
 package com.example.yoga;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try {
-            setContentView(R.layout.activity_main);
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        setContentView(R.layout.activity_main);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
     }
 }
--- app/src/main/java/com/example/yoga/StartScreen.java
+++ app/src/main/java/com/example/yoga/StartScreen.java
@@ -1,19 +1,51 @@
 package com.example.yoga;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ProgressBar;
 
 public class StartScreen extends AppCompatActivity {
 
+    Handler handler;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_screen);
+        handler = new Handler();
+        progressBar= findViewById(R.id.progress);
+
+        Thread welcomeThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(1500);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+
+                    Intent i = new Intent(StartScreen.this,
+                            MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+    }
 }
