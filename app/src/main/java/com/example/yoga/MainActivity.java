75e93db4fa9c248f38a26660299c4588c76994dd
Mutharasan <muthu.ct.96@gmail.com>
2019-01-30 12:12:19 +0530

    Created a ListView for the main menu.

--- app/src/main/java/com/example/yoga/MainActivity.java
+++ app/src/main/java/com/example/yoga/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.yoga;
 
-import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.Window;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ListView listView;
+    String[] menulist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.main_menu);
+        menulist=getResources().getStringArray(R.array.menu_list);
+        MainmenuArrayadapter adapter = new MainmenuArrayadapter(this,menulist);
+        listView.setAdapter(adapter);
     }
 
     @Override
